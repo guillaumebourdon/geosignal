498fac24fbbf29d091fc337383911014ded7d40d
Guillaume Bourdon <guillaume.bourdon5@gmail.com>
2026-04-01 00:30:29 +0100

    Favicon 4 dots Detekia

--- pages/_document.js
+++ pages/_document.js
@@ -78,7 +78,9 @@ export default function Document() {
           })}}
         />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />
