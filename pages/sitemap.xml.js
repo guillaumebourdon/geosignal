0670661e72f3c02c71f3456641ab96006ef360e7
Guillaume Bourdon <guillaume.bourdon5@gmail.com>
2026-03-31 23:30:07 +0100

    Sitemap dynamique : toutes les pages + tous les articles

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,22 +1,46 @@
+import { articles } from '../lib/articles';
+
 const SITE_URL = 'https://detekia.fr';
 
-export default function Sitemap() {}
+function generateSiteMap() {
+  const staticPages = [
+    { loc: '', changefreq: 'weekly', priority: '1.0' },
+    { loc: '/pricing', changefreq: 'monthly', priority: '0.8' },
+    { loc: '/methodologie', changefreq: 'monthly', priority: '0.8' },
+    { loc: '/a-propos', changefreq: 'monthly', priority: '0.7' },
+    { loc: '/contact', changefreq: 'monthly', priority: '0.7' },
+    { loc: '/blog', changefreq: 'daily', priority: '0.9' },
+    { loc: '/mentions-legales', changefreq: 'yearly', priority: '0.3' },
+    { loc: '/cgu', changefreq: 'yearly', priority: '0.3' },
+    { loc: '/confidentialite', changefreq: 'yearly', priority: '0.3' },
+  ];
 
-export async function getServerSideProps({ res }) {
-  const pages = ['', '/pricing', '/contact', '/legal', '/methodologie'];
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const today = new Date().toISOString().split('T')[0];
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(page => `  <url>
-    <loc>${SITE_URL}${page}</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    <changefreq>${page === '' ? 'weekly' : 'monthly'}</changefreq>
-    <priority>${page === '' ? '1.0' : '0.8'}</priority>
+${staticPages.map(p => `  <url>
+    <loc>${SITE_URL}${p.loc}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>${p.changefreq}</changefreq>
+    <priority>${p.priority}</priority>
+  </url>`).join('\n')}
+${articles.map(a => `  <url>
+    <loc>${SITE_URL}/blog/${a.slug}</loc>
+    <lastmod>${a.date || today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
   </url>`).join('\n')}
 </urlset>`;
+}
 
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
   res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate');
   res.write(sitemap);
   res.end();
-
   return { props: {} };
 }
+
+export default function Sitemap() {}
