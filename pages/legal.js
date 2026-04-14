ca4266bcfe262feb7165fc32c7144e897384caac
Guillaume Bourdon <guillaume.bourdon5@gmail.com>
2026-04-14 18:31:56 +0100

    chore: supprimer legal.js (doublon) et api/hello.js (inutile)

--- pages/legal.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-export default function Legal() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/mentions-legales'); }, []);
-  return null;
-}
